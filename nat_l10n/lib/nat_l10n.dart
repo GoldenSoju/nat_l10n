c878ef48e356d98c7192b352df7db115b09c5d17
GoldenSoju <pol82fa91!>
2022-02-26 09:12:51 +0900

    Update documentation

--- nat_l10n/lib/nat_l10n.dart
+++ nat_l10n/lib/nat_l10n.dart
@@ -10,21 +10,26 @@ class NatL10n {
     return Natl10nPlatform.instance;
   }
 
+  /// Returns a list of TimeZoneInfo, localized in the specified Locale.
+  /// The formatting style is optional.
   Future<List<TimeZoneInfo>> localizedTimeZones(
       Locale locale, List<String> timeZones,
       {TimeZoneStyle style = TimeZoneStyle.long}) {
     return _platform.localizedTimeZones(locale, timeZones, style: style);
   }
 
+  /// Returns a list of LocaleInfo, localized in the specified Locale.
   Future<List<LocaleInfo>> localizedLocales(
       Locale locale, List<Locale> locales) {
     return _platform.localizedLocales(locale, locales);
   }
 
+  /// Returns a list of LocaleInfo, localized in each own Locale respectively.
   Future<List<LocaleInfo>> respectivelyLocalizedLocales(List<Locale> locales) {
     return _platform.respectivelyLocalizedLocales(locales);
   }
 
+  /// Returns a list of CurrencyInfo, localized in the specified Locale.
   Future<List<CurrencyInfo>> localizedCurrencies(
       Locale locale, List<String> currencies) {
     return _platform.localizedCurrencies(locale, currencies);
--- nat_l10n_platform_interface/CHANGELOG.md
+++ nat_l10n_platform_interface/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2
+
+- Description update in `pubspec.yaml`.
+
 ## 0.0.1
 
 - Initial release
